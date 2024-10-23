37ec863c2e0f6593c60ae2d980b75adb9607b11a
felimarod <felipemartinrod@gmail.com>
2024-10-22 20:22:36 -0500

    feat: agregar angular/material

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# GestorDeTareasYProyectos
+# Gestor De Tareas Y Proyectos
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.9.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatIcon, MatToolbarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'gestor-de-tareas-y-proyectos';
-}
+export class AppComponent {}
